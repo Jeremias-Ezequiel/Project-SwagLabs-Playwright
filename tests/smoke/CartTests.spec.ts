2b89b9fa49e5227512b3489543f6a31cbb6e80ca
Omonte Jeremias <jeremias150817@gmail.com>
2025-01-20 13:48:32 -0300

    feat: added tests smoke to cart page

---
+++ tests/smoke/CartTests.spec.ts
@@ -0,0 +1,28 @@
+import test, { expect } from "@playwright/test";
+import { CommonFlows } from "../../src/utils/CommonFlows";
+import { CartPage } from "../../src/pages/CartPage";
+import { InventoryPage } from "../../src/pages/InventoryPage";
+
+test.describe('Cart Page Test - Smoke', () => {
+    let commonFlows : CommonFlows;  
+    let cartPage : CartPage; 
+    let inventoryPage : InventoryPage; 
+
+    test.beforeEach(async ({ page }) => {
+        commonFlows = new CommonFlows(page);
+        cartPage = new CartPage(page);  
+        inventoryPage = new InventoryPage(page); 
+
+        await commonFlows.goToCartPage(); 
+    })
+
+    test('Verify cart page', async ({ page }) => {
+        await cartPage.verifyCartPage(); 
+    })
+    
+    test('Verify that there is not products in the cart', async ({ page }) => {
+        const result = await cartPage.verifyElementsInCart();
+        expect(result).toBe(false); 
+    })
+    
+})
