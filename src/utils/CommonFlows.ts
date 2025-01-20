c5110aff5f132b5221e2674fd4be6063f96548b9
Omonte Jeremias <jeremias150817@gmail.com>
2025-01-20 13:47:25 -0300

    feat: added function to go to cart page

--- src/utils/CommonFlows.ts
+++ src/utils/CommonFlows.ts
@@ -5,6 +5,7 @@ import { LoginPage } from '../pages/LoginPage';
 import { validCredential } from '../models/LoginCredentialModel';
 import { NavBarPage } from '../pages/NavBarPage';
 import { InventoryPage } from '../pages/InventoryPage';
+import { CartPage } from '../pages/CartPage';
 
 export class CommonFlows extends BasePage{
     private loginPage : LoginPage;
@@ -12,6 +13,7 @@ export class CommonFlows extends BasePage{
     private navBarPage: NavBarPage;
     private inventoryPage: InventoryPage;
     private inventoryURL : string; 
+    private cartPage : CartPage; 
 
     constructor(page : Page){
         super(page);
@@ -19,6 +21,7 @@ export class CommonFlows extends BasePage{
         this.burguerMenu = new BurgerMenuPage(page);
         this.navBarPage = new NavBarPage(page);
         this.inventoryPage = new InventoryPage(page);
+        this.cartPage = new CartPage(page); 
         this.inventoryURL = 'https://www.saucedemo.com/inventory.html';
     }
 
@@ -27,12 +30,13 @@ export class CommonFlows extends BasePage{
         await this.loginPage.verifyLoginPage();
     }
 
-    async goToInventoryPage() : Promise<void>{
+    private async goToInventoryPage() : Promise<void>{
         await this.page.goto(this.inventoryURL);
         await this.inventoryPage.verifyInventoryPage(); 
     }
 
     async logInSuccessfully() : Promise<void>{
+        await this.goToLoginPage(); 
         const {username,password} = validCredential; 
         await this.loginPage.fillLoginForm(username,password); 
     }
@@ -42,5 +46,11 @@ export class CommonFlows extends BasePage{
         await this.navBarPage.openBurgerMenu();
         await this.burguerMenu.logOut();
         await this.loginPage.verifyLoginPage();
+    }
+
+    async goToCartPage(){
+        await this.logInSuccessfully(); 
+        await this.navBarPage.verifyNavBarPage(); 
+        await this.navBarPage.goToShoopingCartPage();
     }
 }
